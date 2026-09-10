303f25add93d9129909bef35428b89b928c0896d
deletethisbozo-hue <deletethisbozo@gmail.com>
2026-09-10 05:53:09 +0200

    Enable calibrated preview runtime

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { installPreviewRuntime } from "./preview-runtime";
 import "./index.css";
+import "./preview-calibration.css";
+
+installPreviewRuntime();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
