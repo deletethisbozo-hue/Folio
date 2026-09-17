94c013303aa973f599e33e4d094f96fc51dafea1
deletethisbozo-hue <deletethisbozo@gmail.com>
2026-09-17 04:34:30 +0200

    Show Folio 2.0.5 on start screen

--- web/src/StartScreen.tsx
+++ web/src/StartScreen.tsx
@@ -113,7 +113,7 @@ export default function StartScreen(props: { onOpenPath: (path: string) => Promi
     <div className="start-shell" data-ui-tone={tone}>
       <div className="start-windowbar">
         <div className="start-brand">folio</div>
-        <div className="start-version">2.0.4</div>
+        <div className="start-version">2.0.5</div>
       </div>
 
       <main className="start-main">
@@ -166,7 +166,7 @@ export default function StartScreen(props: { onOpenPath: (path: string) => Promi
                         event.preventDefault();
                         event.stopPropagation();
                         setRecent(forgetRecentProject(item.folder));
-                        void api.forgetRecentProject(item.folder).catch(() => {});
+                        void api.forgetRecentProject(folder).catch(() => {});
                       }
                     }}
                   >×</span>
